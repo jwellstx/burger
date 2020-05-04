d7effc26e56a0e1bacbb5534197e1371e9379ab6
JWeez <jwellstx@gmail.com>
2020-05-04 18:51:50 -0500

    connection and comments

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "bc84bda8ba6e91",
-    password: "d7b4e1b6",
+    host: "localhost",
+    user: "root",
+    password: "password",
     port: 3306,
-    database: "heroku_1572b092a276c3e"
+    database: "burgers_db"
 });
 
 connection.connect(err => {
