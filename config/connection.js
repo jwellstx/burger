af879ee6130a60af90255df65f4a4a1081f46566
JWeez <jwellstx@gmail.com>
2020-05-04 19:03:55 -0500

    changing port

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,6 @@ var connection = mysql.createConnection({
     host: "us-cdbr-east-06.cleardb.net",
     user: "bc84bda8ba6e91",
     password: "d7b4e1b6",
-    port: 3306,
     database: "heroku_1572b092a276c3e"
 });
 
