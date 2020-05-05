cd71defd0714d7242c5cc121cc481d797a94bb82
JWeez <jwellstx@gmail.com>
2020-05-04 20:43:20 -0500

    forgot to add port on local connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "bc84bda8ba6e91",
-    password: "d7b4e1b6",
-    database: "heroku_1572b092a276c3e"
+    host: "localhost",
+    user: "root",
+    password: "password",
+    port: 3306,
+    database: "burgers_db"
 });
 
 connection.connect(err => {
