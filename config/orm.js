28176d0161ef39f2b38d3b16619b60b9f221a697
JWeez <jwellstx@gmail.com>
2020-05-04 20:41:39 -0500

    changed connection checking logic

--- config/orm.js
+++ config/orm.js
@@ -3,38 +3,41 @@ var connection = require("./connection.js");
 
 var orm = {
     selectAll: (table, cb) => {
+        this.checkConnection();  // because we are losing connection on heroku
+
         // query here
         var queryString = "SELECT * FROM " + table + ";";
         connection.query(queryString, (err, rows) => {
-            if (err) this.reconnect();  // because we are losing connection on heroku
+            if (err) throw err;
             cb(rows);
         });
     },
     insertOne: (table, burgerType, cb) => {
+        this.checkConnection();  // because we are losing connection on heroku
+
         // query here
         var queryString = "INSERT INTO " + table + " (burger_name, devoured) VALUES (?, 0)";
         connection.query(queryString, burgerType, (err, rows) => {
-            if (err) this.reconnect();  // because we are losing connection on heroku
+            if (err) throw err;
             cb(rows);
         })
     },
     updateOne: (table, id, cb) => {
+        this.checkConnection();  // because we are losing connection on heroku
+
         // query here
         var queryString = "UPDATE " + table + " SET devoured = 1 WHERE id = ?"
         connection.query(queryString, id, (err, rows) => {
-            if (err) this.reconnect();  // because we are losing connection on heroku
+            if (err) throw err;
             cb(rows);
         })
     },
-    reconnect: () => {
-        try {
+    checkConnection: () => {
+        if(connection.state === 'disconnected') {
             connection.connect(err => {
                 if (err) throw "Connection to database failed with err: " + err;
                 console.log("connected as id " + connection.threadId);
             });
-        }
-        catch(error) {
-            throw "Connect to database could not be established!!";
         }
     }
 }
