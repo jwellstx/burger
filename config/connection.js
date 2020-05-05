9a5d6bcd28205bcdf1875f9a11e51867c5e69014
JWeez <jwellstx@gmail.com>
2020-05-04 21:58:58 -0500

    using createPool

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mysql = require("mysql");
 
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
         host: "us-cdbr-east-06.cleardb.net",
         user: "bc84bda8ba6e91",
         password: "d7b4e1b6",
         database: "heroku_1572b092a276c3e"
     });
 
-connection.connect(err => {
-    if (err) throw "err4";
-    console.log("connected as id " + connection.threadId);
-});
+// connection.connect(err => {
+//     if (err) throw err;
+//     console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,48 +1,27 @@
 var connection = require("./connection.js");
 
-
 var orm = {
     selectAll: (table, cb) => {
-        // orm.checkConnection();  // because we are losing connection on heroku
-
-        // query here
         var queryString = "SELECT * FROM " + table + ";";
         console.log(queryString);
         connection.query(queryString, (err, rows) => {
-            if (err) throw "err0";
+            if (err) throw err;
             cb(rows);
         });
     },
     insertOne: (table, burgerType, cb) => {
-        // orm.checkConnection();  // because we are losing connection on heroku
-
-        // query here
         var queryString = "INSERT INTO " + table + " (burger_name, devoured) VALUES (?, 0)";
-        console.log(queryString);
         connection.query(queryString, burgerType, (err, rows) => {
-            if (err) throw "err1";
+            if (err) throw err;
             cb(rows);
         })
     },
     updateOne: (table, id, cb) => {
-        // orm.checkConnection();  // because we are losing connection on heroku
-
-        // query here
         var queryString = "UPDATE " + table + " SET devoured = 1 WHERE id = ?"
-        console.log(queryString);
         connection.query(queryString, id, (err, rows) => {
-            if (err) throw "err2";
+            if (err) throw err;
             cb(rows);
         })
-    },
-    checkConnection: () => {
-        console.log(connection.state);
-        if(connection.state === 'disconnected') {
-            connection.connect(err => {
-                if (err) throw "Connection to database failed with err: " + err;
-                console.log("connected as id " + connection.threadId);
-            });
-        }
     }
 }
 
