0b5de2a17e9ba209ecc90ba8e09f8558c455fd67
JWeez <jwellstx@gmail.com>
2020-05-04 20:42:21 -0500

    heroku db credentials

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "bc84bda8ba6e91",
+    password: "d7b4e1b6",
+    database: "heroku_1572b092a276c3e"
 });
 
 connection.connect(err => {
